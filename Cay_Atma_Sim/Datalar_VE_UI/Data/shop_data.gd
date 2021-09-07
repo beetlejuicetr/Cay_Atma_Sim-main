6147818251b20397a2d44a301ddb4228711edc62
beetlejuicetr <64163239+beetlejuicetr@users.noreply.github.com>
2021-09-07 17:21:17 +0300

    kk

--- Cay_Atma_Sim/Datalar_VE_UI/Data/shop_data.gd
+++ Cay_Atma_Sim/Datalar_VE_UI/Data/shop_data.gd
@@ -1,5 +1,18 @@
 extends Node
 
+
+var txt = {
+	"caycur" : preload("res://Probs/Caylar/textures/caycur.png"),
+	"tipton" : preload("res://Probs/Caylar/textures/tipton_paket.png"),
+	"dodus" : preload("res://Probs/Caylar/textures/dodus_paket.png"),
+	"alpha_tea": preload("res://Probs/Caylar/textures/alphatea_paket.png")
+}
+
+var msh = {
+	"cay1":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay.tres"),
+	"cay2":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay2.tres")
+}
+
 var Sectors = {
 	# bu sector 300 populasyon ile satın alınır
 	#isim|ücret|+populasyon
@@ -8,12 +21,12 @@ var Sectors = {
 	#cay3 580 , 6
 	#cay4 1100 , 10
 	"sector1":{"status":false,"price":300,
-	"cay1":{"status":false,"name":"Caycur","price":150,"texture":preload("res://Probs/Caylar/textures/caycur.png"),"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay.tres"),"pop_plus":2},
-	"cay2":{"status":false,"name":"Tipton","price":320,"texture":preload("res://Probs/Caylar/textures/tipton_paket.png"),"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay.tres"),"pop_plus":4},
-	"cay3":{"status":false,"name":"Doduş","price":580,"texture":preload("res://Probs/Caylar/textures/dodus_paket.png"),"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay.tres"),"pop_plus":6},
-	"cay4":{"status":false,"name":"Alpha Tea","price":1100,"texture":preload("res://Probs/Caylar/textures/alphatea_paket.png"),"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay.tres"),"pop_plus":10}},
+	"cay1":{"status":false,"name":"Caycur","price":150,"texture":txt["caycur"],"mesh":msh["cay1"],"pop_plus":2},
+	"cay2":{"status":false,"name":"Tipton","price":320,"texture":txt["tipton"],"mesh":msh["cay1"],"pop_plus":4},
+	"cay3":{"status":false,"name":"Doduş","price":580,"texture":txt["dodus"],"mesh":msh["cay1"],"pop_plus":6},
+	"cay4":{"status":false,"name":"Alpha Tea","price":1100,"texture":txt["alpha_tea"],"mesh":msh["cay1"],"pop_plus":10}},
 	
-	
+
 	
 	# bu sector 2500 populasyon ile satın alınır
 	#isim|ücret|+populasyon
@@ -22,10 +35,10 @@ var Sectors = {
 	#cay3 5300 , 20
 	#cay4 10500 , 25
 	"sector2":{"status":false,"price":2500,
-	"cay1":{"status":false,"name":"Caycur","price":1200,"texture":null,"pop_plus":12},
-	"cay2":{"status":false,"name":"Tipton","price":2250,"texture":null,"pop_plus":16},
-	"cay3":{"status":false,"name":"Doduş","price":5300,"texture":null,"pop_plus":20},
-	"cay4":{"status":false,"name":"Alpha Tea","price":10500,"texture":null,"pop_plus":25}},
+	"cay1":{"status":false,"name":"Caycur","price":1200,"texture":null,"mesh":msh["cay2"],"pop_plus":12},
+	"cay2":{"status":false,"name":"Tipton","price":2250,"texture":null,"mesh":msh["cay2"],"pop_plus":16},
+	"cay3":{"status":false,"name":"Doduş","price":5300,"texture":null,"mesh":msh["cay2"],"pop_plus":20},
+	"cay4":{"status":false,"name":"Alpha Tea","price":10500,"texture":null,"mesh":msh["cay2"],"pop_plus":25}},
 	
 	
 	
@@ -36,10 +49,10 @@ var Sectors = {
 	#cay3 24100 , 34
 	#cay4 28500 , 40
 	"sector3":{"status":false,"price":12000,
-	"cay1":{"status":false,"name":"Caycur","price":13000,"texture":null,"pop_plus":27},
-	"cay2":{"status":false,"name":"Tipton","price":17050,"texture":null,"pop_plus":30},
-	"cay3":{"status":false,"name":"Doduş","price":24100,"texture":null,"pop_plus":34},
-	"cay4":{"status":false,"name":"Alpha Tea","price":28500,"texture":null,"pop_plus":40}},
+	"cay1":{"status":false,"name":"Caycur","price":13000,"texture":null,"mesh":msh["cay1"],"pop_plus":27},
+	"cay2":{"status":false,"name":"Tipton","price":17050,"texture":null,"mesh":msh["cay1"],"pop_plus":30},
+	"cay3":{"status":false,"name":"Doduş","price":24100,"texture":null,"mesh":msh["cay1"],"pop_plus":34},
+	"cay4":{"status":false,"name":"Alpha Tea","price":28500,"texture":null,"mesh":msh["cay1"],"pop_plus":40}},
 	
 	
 	
@@ -50,10 +63,10 @@ var Sectors = {
 	#cay3 41500 , 50 "mesh":preload(""),
 	#cay4 54500 , 59
 	"sector4":{"status":false,"price":21000,
-	"cay1":{"status":false,"name":"Caycur","price":30000,"texture":null,"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay2.tres"),"pop_plus":42},
-	"cay2":{"status":false,"name":"Tipton","price":36000,"texture":null,"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay2.tres"),"pop_plus":46},
-	"cay3":{"status":false,"name":"Doduş","price":41500,"texture":null,"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay2.tres"),"pop_plus":50},
-	"cay4":{"status":false,"name":"Alpha Tea","price":54500,"texture":null,"mesh":preload("res://Datalar_VE_UI/Shop/cay_meshs/cay2.tres"),"pop_plus":59}},
+	"cay1":{"status":false,"name":"Caycur","price":30000,"texture":null,"mesh":msh["cay2"],"pop_plus":42},
+	"cay2":{"status":false,"name":"Tipton","price":36000,"texture":null,"mesh":msh["cay2"],"pop_plus":46},
+	"cay3":{"status":false,"name":"Doduş","price":41500,"texture":null,"mesh":msh["cay2"],"pop_plus":50},
+	"cay4":{"status":false,"name":"Alpha Tea","price":54500,"texture":null,"mesh":msh["cay2"],"pop_plus":59}},
 	
 }
 
@@ -61,6 +74,7 @@ var Money = 5100
 
 var Current_Cay_Paketi = {
 	"texture":null,
+	"mesh":null,
 	"pop_plus":null
 }
 
--- Cay_Atma_Sim/Probs/Caylar/cay.gd
+++ Cay_Atma_Sim/Probs/Caylar/cay.gd
@@ -2,7 +2,7 @@ extends RigidBody
 # İlahi gücün masumlara yardımı -Ardacorex
 var pop_plus = 0
 onready var vs = get_node("VisibilityNotifier")
-onready var cay_mat = preload("res://Probs/Caylar/Cay0/Material.material")
+onready var cay_mat = preload("res://Probs/Caylar/Cay0/cay_mat.tres")
 func _ready():
 	vs.connect("screen_exited",self,"_ekranda_cikti")
 	if ShopData.Current_Cay_Paketi["texture"] != null:
@@ -10,6 +10,8 @@ func _ready():
 	if ShopData.Current_Cay_Paketi["pop_plus"] != null:
 		pop_plus = ShopData.Current_Cay_Paketi["pop_plus"]
 	print(pop_plus)
+	if ShopData.Current_Cay_Paketi["mesh"] != null:
+		$MeshInstance.mesh = ShopData.Current_Cay_Paketi["mesh"]
 	pass
 
 
